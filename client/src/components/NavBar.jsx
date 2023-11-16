e02a319bc8866cb90e68b243f5cc10fbf8a8816d
Lautaro Bunkowski <lautibunko@gmail.com>
2023-11-16 03:20:50 -0300

    esquema general de las view en la app + Logo + colores

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,10 +1,19 @@
 import { NavLink, Link } from "react-router-dom";
+import logo from "./logo.png";
+import styles from "./NavBar.module.css";
 
 export default function NavBar() {
+  const auth = true;
   return (
-    <div>
-      <Link to={"/"}>GameWorld </Link>
-      <NavLink to={"/home"}>Home</NavLink>
+    <div className={styles.navBar}>
+      <Link to={"/"}>
+        <img src={logo} alt="logo" className={styles.logo} />
+      </Link>
+      <div className={styles.views_container}>
+        <NavLink to={"/home"}>home</NavLink>
+        {auth && <Link to={"/carrito"}>carrito</Link>}
+        {auth && <Link to={"/wishlist"}>wishlist</Link>}
+      </div>
     </div>
   );
 }
