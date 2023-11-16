533b457ba74d326db23cbff75eaa4b301cfeb38f
lautaro_Bunkowski <lautibunko@gmail.com>
2023-11-16 12:03:53 -0300

    maquetado general landing page #1, se implemento secciones para la atracción del cliente, falta estilos a implementar

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -3,16 +3,18 @@ import logo from "./logo.png";
 import styles from "./NavBar.module.css";
 
 export default function NavBar() {
-  const auth = true;
+  const auth = false;
   return (
     <div className={styles.navBar}>
       <Link to={"/"}>
         <img src={logo} alt="logo" className={styles.logo} />
       </Link>
       <div className={styles.views_container}>
-        <NavLink to={"/home"}>home</NavLink>
-        {auth && <Link to={"/carrito"}>carrito</Link>}
-        {auth && <Link to={"/wishlist"}>wishlist</Link>}
+        <NavLink to={"/home"}>Home</NavLink>
+        {auth && <Link to={"/carrito"}>Carrito</Link>}
+        {auth && <Link to={"/wishlist"}>Wishlist</Link>}
+        {auth === false && <Link to={"/login"}>Login | Join</Link>}
+
       </div>
     </div>
   );
--- client/src/views/Landing.jsx
+++ client/src/views/Landing.jsx
@@ -2,9 +2,33 @@ import React from 'react'
 
 const Landing = () => {
   return (
-    <div>
-        <p>Landing</p>
-    </div>
+    <section className="w-screen flex flex-col items-center justify-center text-center">
+        <section className="flex w-10/12 h-[85vh] items-start justify-center">
+          <div className="max-w-[700px] flex items-center gap-8 flex-col mt-[50px]">
+            <h1 className="text-7xl font-bold">LA TIENDA DE GAMERS PARA GAMERS</h1>
+            <p className="text-xl max-w-xs">compra juegos digitales y fisicos al mejor precio del mercado</p>
+            <div className=" w-[420px] flex items-center justify-between">
+              <button className="w-[190px] h-[50px] bg-gradient-to-r from-violet-600 to-orange-600 text-white text-xl">Join now</button>
+              <button className="w-[190px] h-[50px] border-[#8f00ff] border-2 text-xl">Home</button>
+            </div>
+          </div>
+        </section>
+        <section className="flex w-10/12 h-[85vh] items-start justify-center">
+          <div className="flex items-center gap-8 flex-col mt-[50px]">
+            <h1 className="text-7xl font-bold">MORE THAN 1000 GAMES</h1>
+          </div>
+        </section>
+        <section className="flex w-10/12 h-[85vh] items-start justify-center">
+          <div className="flex items-center gap-8 flex-col mt-[50px]">
+            <h1 className="text-7xl font-bold">OF ALL CONSOLES</h1>
+          </div>
+        </section>
+        <section className="flex w-10/12 h-[85vh] items-center justify-center">
+          <div className="flex items-center gap-8 flex-col mt-[50px]">
+            <h1 className="text-7xl font-bold">BECOME A PART OF THE COMMUNITY</h1>
+          </div>
+        </section>
+    </section>
   )
 }
 
