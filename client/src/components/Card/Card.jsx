7f67beaf6b6ed4e3cb56bfa7cbde8536150d0029
Lautaro Bunkowski <lautibunko@gmail.com>
2023-11-17 15:54:11 -0300

    arreglo card #1

--- client/src/components/Card/Card.jsx
+++ client/src/components/Card/Card.jsx
@@ -1,13 +1,12 @@
-import style from "./Card.module.css"
+// import style from "./Card.module.css"
 import { Link } from "react-router-dom";
 
 const Card = ({ game }) => {
-  const {id, plataform, name, image, price } = game;
+  const { id, plataform, name, image, price } = game;
 
   return (
-    <div className={style.card } >
-      <h3>{plataform}</h3>
-      <img src={image} alt="" />
+    <div className="w-[220px] h-[400px] bg-[#1F0A4D] text-white">
+      <img className="w-full h-[250px] object-cover" src={image} alt={name} />
       <Link to={`/detail/${id}`}>
         <h3>{name}</h3>
       </Link>
