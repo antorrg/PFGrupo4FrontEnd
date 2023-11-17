2fdec46a333e003bae6f14a2669903cb08e4ce27
DrFox <daprthefox@gmail.com>
2023-11-17 11:50:19 -0400

    Se importaron los componentes q se modificaron

--- client/src/views/Home.jsx
+++ client/src/views/Home.jsx
@@ -1,9 +1,11 @@
 import React, { useEffect } from "react";
 import styles from "./Home.module.css";
 import NavBar from "../components/NavBar";
+import SearchBar from "../components/SearchBar";
 import Cards from "../components/Cards";
 import { getGames } from "../redux/actions";
 import { useDispatch } from "react-redux";
+import Filters from "../components/Filters";
 
 const Home = () => {
   const dispatch = useDispatch();
@@ -14,10 +16,13 @@ const Home = () => {
   return (
     <div className={styles.home}>
       <div className={styles.filtros_container}>
+        <SearchBar/>
+        <br />
+        <Filters/>
+        {/* <div className={styles.filtros}></div>
         <div className={styles.filtros}></div>
         <div className={styles.filtros}></div>
-        <div className={styles.filtros}></div>
-        <div className={styles.filtros}></div>
+        <div className={styles.filtros}></div> */}
       </div>
       <div className={styles.cards}>
         <Cards />
