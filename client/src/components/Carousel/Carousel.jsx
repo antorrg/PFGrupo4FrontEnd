e1ae25251f19ae36faa833e38492069078e0ed7c
Lautaro Bunkowski <lautibunko@gmail.com>
2023-11-18 15:52:01 -0300

    carrusel section landing

---
+++ client/src/components/Carousel/Carousel.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import Slider from "react-slick";
+
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+import { useSelector } from "react-redux";
+
+const Carousel = () => {
+  const games = useSelector((state) => state.games);
+
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    initialSlide: 0,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    pauseOnHover: true,
+  };
+
+  return (
+    <Slider {...settings} className="flex items-center justify-center">
+      {games.slice(0, 10).map((game, index) => {
+        return (
+          <div className="w-[300px] h-[500px]">
+            <img
+              src={game.image}
+              alt={`imagen-${index}`}
+              className="object-cover h-full w-full"
+            />
+          </div>
+        );
+      })}
+    </Slider>
+  );
+};
+
+export default Carousel;
