95e62df16d017d66cc0013f42df61162f6cac6cf
DrFox <daprthefox@gmail.com>
2023-11-18 01:03:52 -0400

    Se agregó baseUrl como default de axios

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import "./index.css";
 import store, { persistor } from "./redux/store";
 import { PersistGate } from "redux-persist/integration/react";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
+import axios from "axios";
+import App from "./App.jsx";
+import "./index.css";
+
+axios.defaults.baseURL = "http://localhost:3001";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
