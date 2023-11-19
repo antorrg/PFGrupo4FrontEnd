0590627cf84f8208a62547f93f62a90b13c0e096
DrFox <daprthefox@gmail.com>
2023-11-19 00:52:12 -0400

    Se agregó acciones en redux

--- client/src/redux/actions.js
+++ client/src/redux/actions.js
@@ -1,15 +1,17 @@
-
 import axios from "axios";
 
 export const GET_GAMES = "GET_GAMES";
 export const GET_DETAILS = "GET_DETAILS";
 export const CLEAR_DETAILS = "CLEAR_DETAILS";
+export const GET_PLATFORMS = "GET_PLATFORMS";
+export const GET_GENRES = "GET_GENRES";
+export const SEARCH_GAME = "SEARCH_GAME";
 
 export const getGames = () => {
   return async (dispatch) => {
     try {
       const { data } = await axios("/games");
-      console.log(data);
+
       return dispatch({
         type: GET_GAMES,
         payload: data,
@@ -23,7 +25,7 @@ export const getGames = () => {
 export const getDetails = (id) => {
   return async function (dispatch) {
     try {
-      const response = await axios.get(`/${id}`);
+      const response = await axios.get(`/games/${id}`);
       return dispatch({
         type: GET_DETAILS,
         payload: response.data,
@@ -39,3 +41,46 @@ export const clearDetails = () => {
     type: CLEAR_DETAILS,
   };
 };
+
+export const getPlatforms = () => {
+  return async function (dispatch) {
+    try {
+      const response = await axios.get("/platforms");
+      return dispatch({
+        type: GET_PLATFORMS,
+        payload: response.data,
+      });
+    } catch (error) {
+      alert(error);
+    }
+  };
+};
+
+export const getGenres = () => {
+  return async function (dispatch) {
+    try {
+      const response = await axios.get("/genres");
+      return dispatch({
+        type: GET_GENRES,
+        payload: response.data,
+      });
+    } catch (error) {
+      alert(error);
+    }
+  };
+};
+
+export const searchGame = (name) => {
+  return async function (dispatch) {
+    try {
+      const response = await axios.get(`/videogames?name=${name}`);
+      return dispatch({
+        type: SEARCH_GAME,
+        payload: response.data,
+      });
+    } catch (error) {
+      alert(error);
+    }
+  };
+
+}
