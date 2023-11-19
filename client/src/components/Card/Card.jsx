3963b3d7b95eab61fb6948a2e01c4eca03a9c52f
Lautaro Bunkowski <lautibunko@gmail.com>
2023-11-19 02:20:17 -0300

    solucion erroren ruta backend allgames y platafors con info del frontEnd

--- client/src/components/Card/Card.jsx
+++ client/src/components/Card/Card.jsx
@@ -1,12 +1,12 @@
 import { Link } from "react-router-dom";
-import ConsolesLogos from "../ConsolesLogos/ConsolesLogos";
+import ConsoleLogos from "../ConsoleLogos/ConsoleLogos";
 
 const Card = ({ game }) => {
-  const { id, Platforms, name, image, price } = game;
+  const { id, platforms, name, image, price } = game;
   return (
     <div className="w-[220px] h-[400px] bg-[#1F0A4D] text-white flex flex-col">
       <img className="w-full h-[250px] object-cover" src={image} alt={name} />
-      <ConsolesLogos Platforms={Platforms} />
+      <ConsoleLogos platforms={platforms} />
       <div className="text-left p-2 flex-1">
         <Link to={`/detail/${id}`}>
           <h3 className="text-sm font-bold">{name}</h3>
