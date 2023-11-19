8a5d7565af5c736966e1b2992018c8860c072f58
Lautaro Bunkowski <lautibunko@gmail.com>
2023-11-19 01:42:56 -0300

    implementacion landing estilos, detail completacion y componente de logos

---
+++ client/src/components/ConsolesLogos/iconsPlatforms.js
@@ -0,0 +1,114 @@
+const iconsPlatforms = (Platform) => {
+  switch (Platform) {
+    case "PlayStation 1":
+      return "PlayStation";
+    case "PlayStation 2":
+      return "PlayStation";
+    case "PlayStation 3":
+      return "PlayStation";
+    case "PlayStation 4":
+      return "PlayStation";
+    case "PlayStation 5":
+      return "PlayStation";
+    case "PS Vita":
+      return "PlayStation";
+    case "PSP":
+      return "PlayStation";
+    case "Apple Macintosh":
+      return "Apple Macintosh";
+    case "Classic Macintosh":
+      return "Apple Macintosh";
+    case "Apple II":
+      return "Apple Macintosh";
+    case "macOS":
+      return "Apple Macintosh";
+    case "iOS":
+      return "iOS";
+    case "Xbox One":
+      return "Xbox";
+    case "Xbox Series S/X":
+      return "Xbox";
+    case "Xbox 360":
+      return "Xbox";
+    case "Nintendo Switch":
+      return "Nintendo";
+    case "Nintendo 3DS":
+      return "Nintendo";
+    case "Nintendo DS":
+      return "Nintendo";
+    case "Nintendo DSi":
+      return "Nintendo";
+    case "Nintendo 64":
+      return "Nintendo";
+    case "GameCube":
+      return "Nintendo";
+    case "Game Boy Advance":
+      return "Nintendo";
+    case "Game Boy Color":
+      return "Nintendo";
+    case "Game Boy":
+      return "Nintendo";
+    case "SNES":
+      return "Nintendo";
+    case "NES":
+      return "Nintendo";
+    case "Atari":
+      return "Atari";
+    case "Atari 7800":
+      return "Atari";
+    case "Atari 5200":
+      return "Atari";
+    case "Atari 2600":
+      return "Atari";
+    case "Atari Flashback":
+      return "Atari";
+    case "Atari 8-bit":
+      return "Atari";
+    case "Atari ST":
+      return "Atari";
+    case "Atari Lynx":
+      return "Atari";
+    case "Atari XEGS":
+      return "Atari";
+    case "Jaguar":
+      return "Atari";
+    case "Genesis":
+      return "Sega";
+    case "SEGA":
+      return "Sega";
+    case "SEGA Saturn":
+      return "Sega";
+    case "SEGA CD":
+      return "Sega";
+    case "SEGA 32X":
+      return "Sega";
+    case "SEGA Master System":
+      return "Sega";
+    case "Dreamcast":
+      return "Sega";
+    case "Game Gear":
+      return "Sega";
+    case "Commodore / Amiga":
+      return "Commodore-amiga";
+    case "3D0":
+      return "3D0";
+    case "Neo Geo":
+      return "Nintendo";
+    case "Wii U":
+      return "Nintendo";
+    case "Wii":
+      return "Nintendo";
+    case "Web":
+      return "Web";
+    case "Android":
+      return "Android";
+    case "Linux":
+      return "Linux";
+    case "PC":
+      return "Windows";
+    default:
+      break;
+  }
+};
+
+export default iconsPlatforms;
