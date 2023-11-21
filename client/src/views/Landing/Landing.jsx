a44179b0de611d032f5819f0cdb9f34bb48bf456
Lautaro Bunkowski <lautibunko@gmail.com>
2023-11-21 01:26:31 -0300

    se hace la consulta inicial de los videojuegos en app.jsx para traer los mismos en cualquier parte de la pagina al carga y/o recargar

--- client/src/views/Landing/Landing.jsx
+++ client/src/views/Landing/Landing.jsx
@@ -7,27 +7,15 @@ import { Link } from "react-router-dom";
 import AOS from "aos";
 // components -------------------------
 import Carousel from "../../components/Carousel/Carousel.jsx";
-import { getGames } from "../../redux/actions";
-import { useDispatch } from "react-redux";
-
 
 const Landing = () => {
-  const dispatch = useDispatch()
   useEffect(() => {
-    dispatch(getGames({
-      page: 0,
-      platforms: "",
-      genres: "",
-      minPrice: -1,
-      maxPrice: -1,
-      name: ""
-    }));
     AOS.init();
   }, []);
   return (
     <section className="w-[100%] flex flex-col items-center justify-center text-center">
       <section className="relative flex w-full h-[90vh] items-center justify-center bg-[url('./src/views/Landing/all_games.jpg')] bg-cover bg-center">
-        <div class="absolute inset-0 bg-black opacity-50"></div>
+        <div className="absolute inset-0 bg-black opacity-50"></div>
         <div
           data-aos="fade-in"
           className="max-w-[700px] gap-10 flex items-center justify-evenly flex-col z-10"
@@ -58,32 +46,36 @@ const Landing = () => {
       >
         <div className="flex items-center justify-evenly gap-8 flex-col overflow-hidden h-full">
           <h1 className="text-5xl font-bold">ACCEDE A +1000 JUEGOS</h1>
-          <div className="w-full">
-            
-          </div>
-          <p>
-            Mas de 1000 juegos a tu disposición!
-          </p>
+          <div className="w-full"></div>
+          <p>Mas de 1000 juegos a tu disposición!</p>
         </div>
       </section>
       <section
         data-aos="fade-up"
         className="grid grid-cols-2 grid-rows-3	w-10/12 h-[100vh] items-center justify-center"
       >
-        <img src={consoles} alt="consoles" className="col-start-1 col-end-2 row-start-1 row-end-3"/>
+        <img
+          src={consoles}
+          alt="consoles"
+          className="col-start-1 col-end-2 row-start-1 row-end-3"
+        />
         <div className="flex items-center h-full gap-8 flex-col col-start-2 col-end-3 row-start-1 row-end-2 text-right">
           <h1 className="text-5xl font-bold">DE TODAS LAS CONSOLAS</h1>
         </div>
         <div className="flex items-center h-full gap-8 flex-col col-start-1 col-end-2 row-start-3 row-end-4 text-left">
           <h1 className="text-5xl font-bold">EN FORMATO FISICO Y/O DIGITAL</h1>
         </div>
-        <img src={consoles} alt="consoles" className="col-start-2 col-end-3 row-start-2 row-end-4"/>
+        <img
+          src={consoles}
+          alt="consoles"
+          className="col-start-2 col-end-3 row-start-2 row-end-4"
+        />
       </section>
       <section
         data-aos="fade-up"
         className="flex w-full h-[90vh] items-center justify-center bg-[url('./src/views/Landing/community.jpg')] bg-cover bg-center"
       >
-        <div class="absolute inset-0 bg-black opacity-50"></div>
+        <div className="absolute inset-0 bg-black opacity-50"></div>
         <div className="flex items-center gap-8 flex-col mt-[50px] z-10">
           <h1 className="text-7xl font-bold text-white">
             SE PARTE DE LA COMUNIDAD
