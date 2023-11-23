ddb491121382b10c50c3ef10dfa9c55ae0c29ad0
antorrg <114789901+antorrg@users.noreply.github.com>
2023-11-23 02:11:07 -0300

    Update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -7,7 +7,8 @@ import axios from "axios";
 import App from "./App.jsx";
 import "./index.css";
 
-axios.defaults.baseURL = "http://localhost:3001";
+//axios.defaults.baseURL = "http://localhost:3001";
+axios.defaults.baseURL = "BACK_DEPLOY";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
