6356396874dfa49dc796461c8f3d4d883e8c3b26
Lautaro Bunkowski <lautibunko@gmail.com>
2023-11-24 02:01:32 -0300

    se estuvo implementando los estilos mobile de el carrito de compras

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,6 +7,8 @@ import { useDispatch, useSelector } from "react-redux";
 import Footer from "./components/Footer/Footer";
 import NavBar from "./components/NavBar/NavBar";
 import { renderRoutes } from "./routes/index.jsx";
+// NEXT -----------------------------------------------
+import { NextUIProvider } from "@nextui-org/react";
 
 function App() {
   const dispatch = useDispatch();
@@ -31,19 +33,21 @@ function App() {
   }, []);
 
   return (
-    <Suspense fallback={<p>Loading...</p>}>
-      <div className="my-0 mx-auto flex flex-col items-center justify-between min-h-screen">
-        <div
-          className="bg-cover bg-center opacity-70 w-full h-screen absolute -z-10 top-0 left-0"
-          style={backgroundImage}
-        >
-          <div className="bg-gradient-to-t from-white to-transparent w-full h-[50%] bottom-0 absolute"></div>
+    <NextUIProvider>
+      <Suspense fallback={<p>Loading...</p>}>
+        <div className="my-0 mx-auto flex flex-col items-center justify-between min-h-screen">
+          <div
+            className="bg-cover bg-center opacity-70 w-full h-screen absolute -z-10 top-0 left-0"
+            style={backgroundImage}
+          >
+            <div className="bg-gradient-to-t from-white to-transparent w-full h-[50%] bottom-0 absolute"></div>
+          </div>
+          <NavBar />
+          <Routes>{renderRoutes()}</Routes>
+          <Footer />
         </div>
-        <NavBar />
-        <Routes>{renderRoutes()}</Routes>
-        <Footer />
-      </div>
-    </Suspense>
+      </Suspense>
+    </NextUIProvider>
   );
 }
 
