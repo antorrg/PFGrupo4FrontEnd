04f48974230dba23e083a11ac5abc93dfb30e955
Lautaro Bunkowski <lautibunko@gmail.com>
2023-11-28 01:36:28 -0300

    implementacion de estilos navbar con auth0

--- client/src/components/Auth0/LogoutButton.jsx
+++ client/src/components/Auth0/LogoutButton.jsx
@@ -1,13 +1,21 @@
 import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
+import { Button } from "@nextui-org/react";
 
 const LogoutButton = () => {
   const { logout } = useAuth0();
 
   return (
-    <button onClick={() => logout({ logoutParams: { returnTo: window.location.origin } })}>
+    <Button
+      onClick={() =>
+        logout({ logoutParams: { returnTo: window.location.origin } })
+      }
+      color="primary"
+      href="#"
+      variant="flat"
+    >
       Salir
-    </button>
+    </Button>
   );
 };
 
