f0ec1a50c160b235fc9cf54169894fd553e12374
antorrg <114789901+antorrg@users.noreply.github.com>
2023-12-08 18:15:10 -0300

    Update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -8,15 +8,15 @@ import "./index.css";
 import { Auth0Provider } from "@auth0/auth0-react";
 
 const domain = import.meta.env.VITE_AUTH0_DOMAIN;
-const clientId = import.meta.env.VITE_CLIENT_ID;
-// const url = import.meta.env.VITE_URL;
-axios.defaults.baseURL = "http://localhost:3001";
-// axios.defaults.baseURL = "https://backendpf-4vdq.onrender.com";
+const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
+const url = import.meta.env.VITE_URL;
+//axios.defaults.baseURL = "http://localhost:3001";
+axios.defaults.baseURL = url;
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Auth0Provider
-    domain="dev-mg7v1nvcxu1guo4c.us.auth0.com" // {domain}
-    clientId="h8Z6AFVMUx0SPRYIyJHJTHF1ZoD7dXlJ" //{clientId}
+    domain={domain}
+    clientId={clientId}
     authorizationParams={{ redirect_uri: window.location.origin }}
   >
     <Provider store={store}>
