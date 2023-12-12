5c4e60811b417ba9a940b243f501459a5c4b2dcb
antorrg <114789901+antorrg@users.noreply.github.com>
2023-12-12 17:43:53 -0300

    Update Settings.jsx

--- client/src/views/Perfil/Settings/Settings.jsx
+++ client/src/views/Perfil/Settings/Settings.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-import UserEdit from "../../../components/Form/UserEdit";
+//import UserEdit from "../../../components/Form/UserEdit";
 //import PasswordEdit from "../../../components/Form/PasswordEdit";
 const Settings = () => {
   return (
     <div className="w-full my-6 flex-1 flex items-center">
       <div className="max-w-[42rem] w-full mx-auto p-8 text-left lg:max-w-[56rem] dark:bg-[#0B0120] rounded-3xl flex h-fit flex-col">
-        <UserEdit />
+        {/*<UserEdit />*/}
         {/* <PasswordEdit/> */}
       </div>
     </div>
